363de7ee15a032cd6aa4382a3da583c666680025
Denis Nikolaev <d.nikolaev.stc@innopolis.university>
2024-07-22 17:55:05 +0500

    new vetka + hw05

--- src/homeworks/homework05/App.java
+++ src/homeworks/homework05/App.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        /*
+        На вход программы в классе App, методе main подается информация о
+        телевизорах в количестве 10 телевизоров. Считать данные в массив объектов
+        (можно выполнить считывание данных в цикле).
+         */
+
         TV[] tv = new TV[5];
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < tv.length; i++) {
@@ -19,18 +26,27 @@ public class App {
             boolean pwr = sc.nextBoolean();
             tv[i].setPower(pwr);
         }
+        System.out.println("Ваши телевизоры: ")
         for (int i = 0; i < tv.length; i++) {
-
             System.out.println((i + 1) + ". " + tv[i].toString());
         }
+
+        // Считать с клавиатуры число допустимого значения громкости звука maxVolume (рекомендуется ввести с клавиатуры целое число от 50 до 70).
+
         System.out.print("Enter maxVolume: ");
         int maxVolume = sc.nextInt();
+
+        // Далее необходимо в цикле вывести только включенные телевизоры, у которых звук является допустимым (меньшим или равным maxVolume).
+
         System.out.println("Включенные телевизоры с допустимой громкостью: ");
         for (int i = 0; i < tv.length; i++) {
             if ((tv[i].getVolume() <= maxVolume) && (tv[i].isPower() == true)) {
                 System.out.println((i + 1) + ". " + tv[i].toString());
             }
         }
+
+        // Добавить в массив Телевизоров сортировку по номеру канала (по возрастанию).
+
         int z = 0;
         TV exp = new TV();
         for (int j = 0; j < tv.length - 1; j++) {
--- src/homeworks/homework05/TV.java
+++ src/homeworks/homework05/TV.java
@@ -3,6 +3,14 @@ package homeworks.homework05;
 import java.util.Objects;
 
 public class TV {
+
+    /*
+    1. В класс Телевизор добавить поля (если не были добавлены ранее):
+    1) Номер включенного канала — integer;
+    2) Громкость звука — integer (от 0 до 100);
+    3) Признак включенного телевизора — boolean.
+    */
+
     private int channel;
     private int volume;
     private boolean power;
@@ -31,6 +39,10 @@ public class TV {
         this.power = power;
     }
 
+    /*
+    Добавить в класс Телевизор методы equals и hashcode.
+     */
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -43,6 +55,11 @@ public class TV {
     public int hashCode() {
         return Objects.hash(channel, volume, power);
     }
+
+    /*
+    Переопределить метод toString класса Телевизор таким образом, чтобы
+    распечатывались: название класса, все поля класса и их значения.
+    */
 
     @Override
     public String toString() {
